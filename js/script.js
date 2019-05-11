88abc5f63d1c355053e7ce0eceeaaff05aba9f03
GALAXY\Bernhard <bernhard.reinsprecht@gmail.com>
2019-05-11 15:16:25 +0200

    updated index, projects & technologies

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ function imageClick(src) {
     var el = document.getElementById("img-modal");
     el.style.display = "block";
     el.addEventListener("click", () => el.style.display = "none");
-    el.innerHTML = "<img src='"+src+"' style='width:100%;'/>";
+    //el.innerHTML = "<div id='img-modal-wrap' style='background-image: url('"+src+"')'></div>";
+    el.innerHTML = `<div id="img-modal-wrap" style="background-image: url('${src}')"></div`;
+    //<img src='"+src+"' style='width:100%;'/>
 }
 
 window.onload = function () {
-    var images = document.getElementsByTagName("img");
+    var images = document.getElementsByClassName("project-image");
     for (let i = 0; i < images.length; i++) {
         const img = images[i];
         img.addEventListener("click", () => imageClick(img.src));
